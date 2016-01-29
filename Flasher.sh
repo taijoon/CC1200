474658193d74394cd0f5bdc672ded561b0c435e5
taijoon <windarin@gmail.com>
2016-01-28 17:07:24 -0800

    Jtag VCC=3000

--- Flasher.sh
+++ Flasher.sh
@@ -3,7 +3,8 @@
 export LD_LIBRARY_PATH=~/MSP430Flasher_1.3.7/
 
 #clear
-~/MSP430Flasher_1.3.7/MSP430Flasher -w "./build/telosz/main.txt" -v -g -z [VCC=3300]
+#~/MSP430Flasher_1.3.7/MSP430Flasher -w "./build/telosz/main.txt" -z [VCC=3300]
+~/MSP430Flasher_1.3.7/MSP430Flasher -w "./build/telosz/main.txt" -z [VCC=3000]
 #read -p "Press any key to continue..."
 #./MSP430Flasher -r [Firmware Output.txt,MAIN]
 #read -p "Press any key to continue..."
